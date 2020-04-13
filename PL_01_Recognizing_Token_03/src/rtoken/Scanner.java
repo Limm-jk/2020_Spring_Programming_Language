394fd0f6fcd655f2abf84ac2c3c5a3ef6b8fe1cf
Limm-jk <201602057@cs-cnu.org>
2020-04-13 23:41:49 +0900

    add initTM

--- PL_01_Recognizing_Token_03/src/rtoken/Scanner.java
+++ PL_01_Recognizing_Token_03/src/rtoken/Scanner.java
@@ -1,8 +1,7 @@
 package rtoken;
 
 import java.io.*;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Scanner {
 
@@ -44,7 +43,29 @@ public class Scanner {
 	
 	private void initTM() {
 		// TODO Auto-generated method stub
-		
+		// 97 ~ 122 소문 03일떄 3
+		// 65 ~ 90 대문 3일때 3
+		// 48 ~ 57 숫 012일떄 2 3일때 3
+		// 45 = - 0일때 1
+		for(int i = 0; i<128 ; i++){
+			for(int j = 0; j< 4; j++){
+				transM[j][i] = -1;//해당되지 않는 모든 entry는 -1
+			}
+		}
+		for(int i = 97; i< 123; i++){
+			transM[0][i] = 3;
+			transM[3][i] = 3;
+		}
+		for(int i = 65; i< 91; i++){
+			transM[3][i] = 3;
+		}
+		for(int i = 48; i< 58; i++){
+			transM[0][i] = 2;
+			transM[1][i] = 2;
+			transM[2][i] = 2;
+			transM[3][i] = 3;
+		}
+		transM[0][45] = 1;
 	}
 	private Token nextToken() {
 		int stateOld = 0 , stateNew;
@@ -58,8 +79,14 @@ public class Scanner {
 		for(int i = 0; i<temp.length(); i++) {
 			//과제
 			//하나씩 TM을 이용하여 판별
+			stateNew = transM[stateOld][temp.charAt(i)];
 			// reject상태 예외처리(메세지 출력후 리턴)
+			if(stateNew == -1) {
+				System.out.println("Error");
+				return result;
+			}
 			// 업데이트
+			stateOld = stateNew;	
 		}
 		for(TokenType t : TokenType.values()) {
 			if(t.finalState == stateOld) {
@@ -72,18 +99,24 @@ public class Scanner {
 	
 	public List<Token> tokenize(){
 		//과제
-		//입력된 모든 token에 대하여 nexttoken으로 식별 후 리스트에 추가하여 반환
-		return null;
+		//입력된 모든 token에 대하여 nextToken으로 식별 후 리스트에 추가하여 반환
+		List<Token> to = new ArrayList<>();
+		Token next = nextToken();
+		while(next != null) {
+			to.add(next);
+			next = nextToken();
+		}
+		return to;
 	}
 
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-		FileReader fr =  new FileReader("C:/as03.txt");
+		FileReader fr =  new FileReader("./as03.txt");
+		@SuppressWarnings("resource")
 		BufferedReader bf = new BufferedReader(fr);
 		String source = bf.readLine();
 		Scanner s = new Scanner(source);
 		List<Token> tokens = s.tokenize();
 		System.out.println(tokens);
 	}
-
 }
